7e901a4b48389bdc1b7d8fa013ed512c81b83618
Thomas Regourd <tom.reg@hotmail.fr>
2022-07-15 17:49:35 +0200

    auth ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthGardService } from './services/auth-gard.service';
 import { SingleBookComponent } from './book-list/single-book/single-book.component';
 import { BookFormComponent } from './book-form/book-form.component';
 import { BookListComponent } from './book-list/book-list.component';
@@ -9,6 +10,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'auth/signup',
+
     component: SignUpComponent,
   },
   {
@@ -17,16 +19,21 @@ const routes: Routes = [
   },
   {
     path: 'books',
+    canActivate: [AuthGardService],
     component: BookListComponent,
   },
   {
     path: 'books/new',
+    canActivate: [AuthGardService],
     component: BookFormComponent,
   },
   {
     path: 'books/view/:id',
+    canActivate: [AuthGardService],
     component: SingleBookComponent,
   },
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  { path: '**', redirectTo: 'books' },
 ];
 
 @NgModule({
--- src/app/services/auth-gard.service.ts
+++ src/app/services/auth-gard.service.ts
@@ -1,9 +1,26 @@
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthGardService {
+export class AuthGardService implements CanActivate {
+  canActivate(): boolean | Observable<boolean> | Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          resolve(true);
+        } else {
+          this.router.navigate(['/auth', 'signin']);
+          resolve(false);
+        }
+      });
+    });
+  }
 
-  constructor() { }
+  constructor(private router: Router) {}
 }
