1cc2c4a4fb0651bb5aa168b7077a2ec99d545d90
Thomas Regourd <tom.reg@hotmail.fr>
2022-07-19 12:06:47 +0200

    books services with firebase

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,15 +1,13 @@
+import { BooksService } from './../services/books.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.scss']
+  styleUrls: ['./book-list.component.scss'],
 })
 export class BookListComponent implements OnInit {
+  constructor(private booksService: BooksService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,9 +1,73 @@
+import { AppComponent } from './../app.component';
+import { Book } from './../models/book.model';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import {
+  getDatabase,
+  ref,
+  set,
+  onValue,
+  DataSnapshot,
+} from 'firebase/database';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BooksService {
+  private books: Book[] = [
+    { author: 'John', title: 'John' },
+    { author: 'Doe', title: 'Doe' },
+  ];
+  bookSubject = new Subject<Book[]>();
 
-  constructor() { }
+  constructor() {}
+
+  emitBooks() {
+    this.bookSubject.next(this.books.slice());
+  }
+
+  saveBooks() {
+    const db = getDatabase();
+    set(ref(db, '/books'), this.books);
+  }
+
+  getBooks() {
+    const db = getDatabase();
+    const books = ref(db, '/books');
+    onValue(books, (snapshot: DataSnapshot) => {
+      this.books = snapshot.val() ? snapshot.val() : [];
+      this.emitBooks();
+    });
+  }
+
+  getSingleBook(id: number) {
+    const db = getDatabase();
+    const book = ref(db, '/books/' + id);
+    onValue(
+      book,
+      (snapshot: DataSnapshot) => {
+        return snapshot.val() ? snapshot.val() : [];
+      },
+      { onlyOnce: true }
+    );
+  }
+
+  createBook(newBook: Book) {
+    this.books.push(newBook);
+    this.saveBooks();
+    this.emitBooks();
+  }
+
+  removeBook(bookToDelete: Book) {
+    const bookToDeleteIndex = this.books.findIndex((book: Book) => {
+      if (book === bookToDelete) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    this.books.splice(bookToDeleteIndex, 1);
+    this.saveBooks();
+    this.emitBooks();
+  }
 }
